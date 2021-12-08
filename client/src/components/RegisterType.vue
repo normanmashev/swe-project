04e2891084ce5ba2a5bfa5ea62285e44ce2d1814
Nariman Imashev <imashnariman@gmail.com>
2021-12-08 11:18:47 +0600

    upd: registration

---
+++ client/src/components/RegisterType.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-card max-width="500px" width="300px">
+		<v-card-title>
+			<h5 class="text-h5">Registration</h5>
+		</v-card-title>
+		<v-card-text>
+			<p class="subtitle-1 text-center">I want to register as</p>
+			<div class="d-flex flex-column">
+				<v-btn
+					class="primary mb-2"
+					depressed
+					@click="$store.commit('register/SET_TYPE', 1)"
+					>Guest</v-btn
+				>
+				<v-btn
+					class="blue white--text"
+					depressed
+					@click="$store.commit('register/SET_TYPE', 2)"
+					>Hotel manager</v-btn
+				>
+			</div>
+		</v-card-text>
+	</v-card>
+</template>
+
+<script>
+export default {
+	name: "RegisterType",
+};
+</script>
+
+<style></style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,44 @@
+<template>
+	<section class="register">
+		<RegisterType v-if="!type" />
+		<RegisterGuest v-else-if="type === 1" />
+		<RegisterManager v-else />
+	</section>
+</template>
+
+<script>
+import RegisterType from "@/components/RegisterType";
+import RegisterGuest from "@/components/RegisterGuest";
+import RegisterManager from "@/components/RegisterManager";
+
+import { mapGetters } from "vuex";
+
+export default {
+	name: "Register",
+
+	components: {
+		RegisterManager,
+		RegisterType,
+		RegisterGuest,
+	},
+
+	computed: {
+		...mapGetters({
+			type: "register/getType",
+		}),
+	},
+
+	beforeCreate() {
+		this.$store.commit("register/SET_TYPE", 0);
+	},
+};
+</script>
+
+<style scoped lang="scss">
+.register {
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
