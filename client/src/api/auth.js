6d48b30c52b598e03d020adecbe85d7b93e3506d
Nariman Imashev <imashnariman@gmail.com>
2021-12-09 02:15:21 +0600

    upd: routes data

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,19 +1,21 @@
 import request from "./request";
 
-export const Login = () =>
+export const Login = data =>
 	request({
 		url: "/login",
 		method: "post",
+		data,
 	})
 		.then(res => res.data)
 		.catch(e => {
 			throw e;
 		});
 
-export const Register = () =>
+export const Register = data =>
 	request({
 		url: "/register",
 		method: "post",
+		data,
 	})
 		.then(res => res.data)
 		.catch(e => {
