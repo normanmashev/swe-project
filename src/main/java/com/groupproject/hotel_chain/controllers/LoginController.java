6cc075f3806cdccd5a30b50b9d80e1e3be73d3ba
IslamGallam <mr.gallam@gmail.com>
2021-12-09 17:52:06 +0600

    Update LoginController.java

--- src/main/java/com/groupproject/hotel_chain/controllers/LoginController.java
+++ src/main/java/com/groupproject/hotel_chain/controllers/LoginController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 
 @CrossOrigin
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class LoginController {
     @Autowired
     GuestRepository guestRepository;
